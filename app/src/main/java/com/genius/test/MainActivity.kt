238e5289d6ba20899a159e51c576e488521cdb0a
Professional <evgeniupetrove@gmail.com>
2021-06-04 07:27:12 +0300

    First Commit

--- app/src/main/java/com/genius/test/MainActivity.kt
+++ app/src/main/java/com/genius/test/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.genius.test
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.genius.test.LoginViewModel.LoginViewModel
+import android.widget.TextView
+import com.genius.test.database.User
+import com.genius.test.database.UserDB
+
 import com.google.firebase.auth.FirebaseAuth
 
-private lateinit  var mAuth: FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+
+  var users: List<User>?  =null
+
+ var singleUser: User? = null;
+
+lateinit var text: TextView
 
 var isExist = false
 
-lateinit var loginViewModel: LoginViewModel
-
+private lateinit  var mAuth: FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,34 +32,34 @@ class MainActivity : AppCompatActivity() {
 
         val user = mAuth.currentUser
 
-        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        text = findViewById(R.id.text)
 
-        user!!.displayName?.let { loginViewModel.getLoginDetails(this, it) }!!.observe(this, Observer {
-
-            if (it == null) {
-                Toast.makeText(this@MainActivity, "User not found", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this@MainActivity, "User not found", Toast.LENGTH_LONG).show()
-            }
-        })
 
             /**If user is not authenticated, send him to SignInActivity to authenticate first.
              * Else send him to DashboardActivity*/
 
-            /**If user is not authenticated, send him to SignInActivity to authenticate first.
-             * Else send him to DashboardActivity*/
             Handler().postDelayed({
-                if (user != null) {
+                if (user == null ) {
 
-                    val dashboardIntent = Intent(this, DashboardActivity::class.java)
-                    startActivity(dashboardIntent)
-                    finish()
 
-                } else {
                     val signInIntent = Intent(this, SignInActivity::class.java)
                     startActivity(signInIntent)
                     finish()
-                }
-            }, 2000)
 
-        }}
+                }
+
+                    else {
+
+                    val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                    startActivity(dashboardIntent)
+                    finish()}
+
+
+            }, 5000)
+
+        }
+
+    private fun launch(block: suspend CoroutineScope.() -> Unit) {
+
+    }
+}
